df48805d746ddfda6d09a7dc64decae38ca7c6ac
Marcin Kumorek <kumeek@gmail.com>
2022-09-13 13:27:40 +0200

    Add Comment plugin

--- lua/qmmr/plugins.lua
+++ lua/qmmr/plugins.lua
@@ -89,6 +89,14 @@ return packer.startup(function(use)
     tag = 'nightly' -- optional, updated every week. (see issue #1193)
   }
 
+  -- Comment
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
