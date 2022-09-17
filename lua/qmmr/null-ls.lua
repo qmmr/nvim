cfe57f5874695d4099d79603a96c05c6ca73e3c6
Marcin Kumorek <kumeek@gmail.com>
2022-09-17 21:54:56 +0200

    null-ls format on save

--- lua/qmmr/null-ls.lua
+++ lua/qmmr/null-ls.lua
@@ -8,6 +8,15 @@ local formatting = null_ls.builtins.formatting
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+local callback = function()
+    vim.lsp.buf.format({
+        bufnr = bufnr,
+        filter = function(client)
+            return client.name == "null-ls"
+        end
+    })
+end
+
 null_ls.setup({
   sources = {
     formatting.prettierd
@@ -18,27 +27,16 @@ null_ls.setup({
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({
-            bufnr = bufnr,
-            filter = function()
-              return client.name == "null-ls"
-            end
-          })
-
-        end,
+        callback = callback
       })
     end
 
     if client.server_capabilities.documentFormattingProvider then
-      vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.format({ async = true })<CR>")
-
-      -- format on save
-      vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.format({ async = true })")
+      vim.cmd("nnoremap <silent><buffer> <leader>f :lua vim.lsp.buf.format({  filter = function(client) return client.name == \"null-ls\" end })<CR>")
     end
 
     if client.server_capabilities.documentRangeFormattingProvider then
-      vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
+      vim.cmd("xnoremap <silent><buffer> <leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
     end
   end,
 })
